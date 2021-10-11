f5a2a9b41c3f7e7934ba2dfab4f06773d06522a1
harmolipi <nikob381@gmail.com>
2021-10-10 21:44:23 -0400

    Added functionality to find photos by user id

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,8 @@
 class StaticPagesController < ApplicationController
-  def home
+  def index
     flickr = Flickr.new
+    unless params[:flickr_profile_id].nil? || params[:flickr_profile_id].empty?
+      @photos = flickr.people.getPublicPhotos(user_id: params[:flickr_profile_id])
+    end
   end
 end
